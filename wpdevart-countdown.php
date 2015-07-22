001b2882752bcd110ed3795a9360374a04141663
wpdevart <wpdevart@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-22 16:15:39 +0000

    add LICENSE file
    git-svn-id: https://plugins.svn.wordpress.org/widget-countdown/trunk@1204083 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wpdevart-countdown.php
+++ wpdevart-countdown.php
@@ -3,7 +3,7 @@
 Plugin Name: Countdown Wpdevart 
 Plugin URI: http://wpdevart.com/wordpress-countdown-plugin/
 Description: WordPress Countdown plugin is an nice tool to create and insert countdown timers into your posts/pages and widgets
-Version: 1.0.1
+Version: 1.0.2
 Author: wpdevart
 Author URI: http://wpdevart.com
 License: GPL3
